2a5d8d2654d34affdb04e60c9109ee732274eb4b
Xiong <xzhao094.my@gmail.com>
2026-06-26 02:04:55 -0500

    updated onset detection and marker selection

--- modules/kinematics/items/pointitem.py
+++ modules/kinematics/items/pointitem.py
@@ -4,9 +4,14 @@ from ..materials.pointmaterial import PointMaterial
 
 
 class PointItem(Mesh):
-    def __init__(self, p=None):
+    def __init__(self, p=None, colors=None):
         self.geo = Geometry()
         self.geo.addAttribute("vec3", "vertexPosition", p)
+        if colors is not None:
+            self.geo.addAttribute("vec3", "vertexColor", colors)
         self.geo.countVertices()
-        mat = PointMaterial({"baseColor": [1, 1, 1]})
+        if colors is not None:
+            mat = PointMaterial({"useVertexColors": True})
+        else:
+            mat = PointMaterial({"baseColor": [1, 1, 1]})
         super().__init__(self.geo, mat)
